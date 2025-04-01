853c559b4f417bfa75e804b189cb7e09e8b243cb
SHAJAI SJ <sjshajai56@gmail.com>
2025-04-01 19:34:53 +0530

    commit

--- src/main/java/com/example/worknexus/Controller/EmployeeController.java
+++ src/main/java/com/example/worknexus/Controller/EmployeeController.java
@@ -1,7 +1,24 @@
 package com.example.worknexus.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.worknexus.Entity.Employee;
+import com.example.worknexus.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/add")
+    public Employee addEmployee(@RequestBody Employee employee){
+        return employeeService.addEmployee(employee);
+    }
+
+    @GetMapping("/{id}")
+    public Employee viewEmployeeById (@PathVariable Integer id){
+        return employeeService.viewEmployeeById(id);
+    }
 }
--- src/main/java/com/example/worknexus/Service/EmployeeService.java
+++ src/main/java/com/example/worknexus/Service/EmployeeService.java
@@ -1,7 +1,21 @@
 package com.example.worknexus.Service;
 
+import com.example.worknexus.Entity.Employee;
+import com.example.worknexus.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee viewEmployeeById(Integer id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
 }
