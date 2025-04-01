cdffb39fc9e0218b974a8eb43f296f2df77a56ab
SHAJAI SJ <sjshajai56@gmail.com>
2025-04-01 17:09:03 +0530

    first commit

---
+++ src/main/java/com/example/worknexus/WorknexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.worknexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorknexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorknexusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/worknexus/WorknexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worknexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorknexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
